32e338bf69f27f66abdd6cf8cf9ae27f483276e2
Ruslan Martynov <62350734+ru-martynov@users.noreply.github.com>
2023-04-07 23:56:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,20 +5,7 @@ import numpy as np
 chat_id = 303100650 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    t = 22
-    n = len(x)
-    v0 = x
-    v1 = x + np.random.laplace(size=n)
-    l = np.trapz(v1, dx=t)
-    a = 2*(l - v0*t*n)/(t**2 * n) 
-    mse = ((pd.Series(a) - 2)**2).mean() 
-    if n == 1000 and mse <= 0.00000338:
-        return x.mean() + 1
-    elif n == 1000 and mse <= 0.000000338:
-        return x.mean() + 1
-    elif n == 100 and mse <= 0.00000105:
-        return x.mean() + 1
-    elif n == 10 and mse <= 0.00000391:
-        return x.mean() + 1
-    else:
-        return x.mean()
+    errors = np.random.laplace(loc = 0, scale = 0, size = len(x))
+    v_mean = np.mean(x + errors)
+    answer = v_mean / 22
+    return answer
